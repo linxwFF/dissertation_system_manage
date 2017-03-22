62f27209c427832c07d120c5d9783e93350f974b
LinxwFF <874226876@qq.com>
2017-03-22 15:55:26 +0800

    教职工表信息表，学生基本数据表，班级数据表的迁移

---
+++ database/migrations/2017_03_22_072254_create_studentBase_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentBaseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studentBaseInfo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',30)->comment('姓名');
+            $table->string('name_spell',60)->comment('姓名拼音');
+            $table->string('student_QQ',20)->comment('学生QQ');
+            $table->string('student_phone',20)->comment('学生电话');
+            $table->string('name_before',30)->comment('曾用名');
+            $table->string('sex_code',1)->comment('性别码');
+            $table->string('birthday',8)->comment('出生日期');
+            $table->string('birth_addr_code',6)->comment('出生地码');
+            $table->string('native_place',20)->comment('籍贯');
+            $table->string('nation_code',2)->comment('民族码');
+            $table->string('nationility_code',3)->comment('国籍/地区码');
+            $table->string('identity_type',1)->comment('身份证件类型码');
+            $table->string('identity_number',20)->comment('身份证件号');
+            $table->string('marriage_states_code',2)->comment('婚姻状况码');
+            $table->string('conuntrymen_code',2)->comment('港澳台侨外码');
+            $table->string('politics_status',1)->comment('政治面貌');
+            $table->string('health_status',1)->comment('健康状态码');
+            $table->string('religion',1)->comment('信仰宗教码');
+            $table->string('blood_type',2)->comment('血型码');
+            $table->string('photo')->comment('照片');
+            $table->string('identity_valid',17)->comment('身份证件有效期');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studentBaseInfo');
+    }
+}
---
+++ database/migrations/2017_03_22_074214_create_classesInfo_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classesInfo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('classes')->comment('班級');
+            $table->string('date_build')->comment('建班年月');
+            $table->string('class_directorID')->comment('班主任教工号');
+            $table->string('monitorID')->comment('班长学号');
+            $table->string('instructorID')->comment('辅导员号');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classesInfo');
+    }
+}
